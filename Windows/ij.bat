007b7cca391007ba6442ec90147ebf880a3a908f
Yusuke Takeda <61795460+ytakeda-paleo@users.noreply.github.com>
2024-11-18 16:25:41 +0900

    Update ij.bat

--- Windows/ij.bat
+++ Windows/ij.bat
@@ -1,4 +1,7 @@
 @echo off
+REM Get the current directory path
+set CURRENT_DIRECTORY=%cd%
+
 REM Check if %1 is empty
 if "%~1"=="" (
     echo Empty file/directory path!
@@ -8,7 +11,6 @@ if "%~1"=="" (
     echo ij [dir_path] v : open an image directory as a virtual stack sequence
     echo ij [dir_path] [i] [j] : open ith - jth images in a directory as a sequence
     echo ij [dir_path] [i] [j] v : open ith - jth images in a directory as a virtual stack sequence
-    
     exit /b
 )
 
@@ -17,32 +19,60 @@ if not "%~5"=="" (
     exit /b
 )
 
+REM Resolve the absolute path of the argument
+set INPUT_PATH=%~1
+set ABSDIR=""
 
+REM Check if the argument exists as a file
+if exist "%INPUT_PATH%" (
+    for %%f in ("%INPUT_PATH%") do (
+        if exist "%%~ff" (
+            set ABSDIR=%%~ff
+        )
+    )
+)
+
+REM Check if the argument exists as a directory
+if exist "%INPUT_PATH%\" (
+    for %%d in ("%INPUT_PATH%\") do (
+        if exist "%%~fd" (
+            set ABSDIR=%%~fd
+        )
+    )
+)
+
+REM Build the command string based on input arguments
 if "%~2"=="" (
-    set command=%1
-
+    set command=%ABSDIR%
 ) else if "%~2"=="v" (
-    set command=%1@@%2
-
+    set command=%ABSDIR%@@%2
 ) else if "%~3"=="" (
     echo Error! Type "ij" to show usage.
     exit /b
-
 ) else if "%~3"=="v" (
     echo Error! Type "ij" to show usage.
     exit /b
-
 ) else if "%~4"=="v" (
-        set command=%1@@%2@%3@%4
-
+    set command=%ABSDIR%@@%2@%3@%4
 ) else if "%~4"=="" (
-    set command=%1@@%2@%3
-
+    set command=%ABSDIR%@@%2@%3
 ) else (
     echo Error! Type "ij" to show usage.
     exit /b
 )
 
+cd /d %~dp0
+
+REM Ensure ImageJ.exe exists
+if not exist "ImageJ.exe" (
+    echo Error: ImageJ.exe not found in %~dp0
+    cd /d %CURRENT_DIRECTORY%
+    exit /b
+)
 
 REM Start ImageJ with the given argument
-start ImageJ.exe -macro %~p0open-imagej.ijm %command%
+start ImageJ.exe -macro open-imagej.ijm %command%
+
+REM Return to the original directory
+cd /d %CURRENT_DIRECTORY%
+
